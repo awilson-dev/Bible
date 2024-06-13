5bbc53b18e58d45dbd3bc6ba948773375dbbbcb9
awilson-dev <awilson-dev@tech-center.com>
2024-06-13 11:09:39 -0400

    Initial Commit

---
+++ Bible/BibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleApp.swift
+//  Bible
+//
+//  Created by Allen Wilson on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct BibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
